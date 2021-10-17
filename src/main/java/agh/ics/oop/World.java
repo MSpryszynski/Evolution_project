60fe90428142380b7fc353dddf7bbfe5f7db638a
Marcel Spryszyński <marcel.spryszynski@onet.pl>
2021-10-17 11:40:21 +0200

    Switch case bazujący na enumie dodany

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -3,15 +3,21 @@ package agh.ics.oop;
 public class World {
     public static void main(String[] args) {
         System.out.println("System wystartował");
-        String[] arguments = {"F", "B"};
+        Direction[] arguments = {Direction.FORWARD, Direction.BACKWARD, Direction.RIGHT, Direction.LEFT, Direction.LEFT};
         run(arguments);
         System.out.println("Sysytem skończył działanie");
     }
-
-    public static void run(String[] arguments){
-        System.out.println("Zwierzak idzie do przodu");
+    public static void run(Direction[] arguments){
+        for (Direction argument: arguments) {
+            switch (argument){
+                case FORWARD -> System.out.println("Zwierzak idzie do przodu");
+                case BACKWARD -> System.out.println("Zwierzak idzie do tyłu");
+                case RIGHT -> System.out.println("Zwierzak idzie w prawo");
+                case LEFT -> System.out.println("Zwierzak idzie w lewo");
+            }
+        }
         int counter = 0;
-        for (String argument: arguments) {
+        for (Direction argument: arguments) {
             counter += 1;
             if (counter != arguments.length){
                 System.out.printf("%s, ", argument);
