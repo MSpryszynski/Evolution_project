0644a83496df135f04755c040967746dfc4bd340
Marcel Spryszyński <marcel.spryszynski@onet.pl>
2021-10-17 11:37:10 +0200

    Dodano argumenty oraz ich wypisywanie do metody run

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -3,10 +3,22 @@ package agh.ics.oop;
 public class World {
     public static void main(String[] args) {
         System.out.println("System wystartował");
-        run();
+        String[] arguments = {"F", "B"};
+        run(arguments);
         System.out.println("Sysytem skończył działanie");
     }
-    public static void run(){
-        System.out.println("Zwierzak idzie fo przodu");
+
+    public static void run(String[] arguments){
+        System.out.println("Zwierzak idzie do przodu");
+        int counter = 0;
+        for (String argument: arguments) {
+            counter += 1;
+            if (counter != arguments.length){
+                System.out.printf("%s, ", argument);
+            }
+            else {
+                System.out.println(argument);
+            }
+        }
     }
 }
