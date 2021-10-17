9ba2268488d659c07a30befa562a14e56def09e8
Marcel Spryszyński <marcel.spryszynski@onet.pl>
2021-10-17 11:34:02 +0200

    Stworzono metodę run

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -3,6 +3,10 @@ package agh.ics.oop;
 public class World {
     public static void main(String[] args) {
         System.out.println("System wystartował");
+        run();
         System.out.println("Sysytem skończył działanie");
+    }
+    public static void run(){
+        System.out.println("Zwierzak idzie fo przodu");
     }
 }
